74b6ca19a9713eb9de7ae7a3510be9590ecb6208
purupuruputtyan <pika0404_minako@yahoo.co.jp>
2023-04-03 06:58:26 +0000

    [Add]応用課題８bの途中

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,15 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_2days, -> { where(created_at: 2.days.ago.all_day) }
+  scope :created_3days, -> { where(created_at: 3.days.ago.all_day) }
+  scope :created_4days, -> { where(created_at: 4.days.ago.all_day) }
+  scope :created_5days, -> { where(created_at: 5.days.ago.all_day) }
+  scope :created_6days, -> { where(created_at: 6.days.ago.all_day) }
+
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
